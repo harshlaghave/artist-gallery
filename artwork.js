177001b005c6f938c147b23f992457849ecc272d
Harsh Laghave <145957194+harshlaghave@users.noreply.github.com>
2025-10-03 21:11:03 +0530

    Update artwork.js

--- artwork.js
+++ artwork.js
@@ -1,23 +1,24 @@
 const artworks = {
     alpenglow: {
       title: "Alpenglow",
-      price: 200,
+      price: 2000,
       image: "images/alpenglow.png",
       desc: "A mesmerizing artwork inspired by glowing mountains during sunset.",
       size:"Giclee,Edition Size: 45 + 5 Artist Proofs ,Image: 21 x 14"
     },
     sunset: {
       title: "Sunset",
-      price: 180,
+      price: 6000,
       image: "images/sunset.png",
       desc: "A vibrant sunset landscape painting.",
       size:"Giclee,Edition Size: 45 + 5 Artist Proofs ,Image: 21 x 14"
     },
     moonlight: {
       title: "Moonlight",
-      price: 250,
+      price: 5000,
       image: "images/moonlight.png",
       desc: "A calm moonlit scenery on canvas.",
       size:"Giclee,Edition Size: 45 + 5 Artist Proofs ,Image: 21 x 14"
     }
+
   };
