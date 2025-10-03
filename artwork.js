593a2e00f76057b62c63d7cf25de0800278955e8
Harsh Laghave <145957194+harshlaghave@users.noreply.github.com>
2025-10-03 20:37:54 +0530

    Add files via upload

---
+++ artwork.js
@@ -0,0 +1,23 @@
+const artworks = {
+    alpenglow: {
+      title: "Alpenglow",
+      price: 200,
+      image: "images/alpenglow.png",
+      desc: "A mesmerizing artwork inspired by glowing mountains during sunset.",
+      size:"Giclee,Edition Size: 45 + 5 Artist Proofs ,Image: 21 x 14"
+    },
+    sunset: {
+      title: "Sunset",
+      price: 180,
+      image: "images/sunset.png",
+      desc: "A vibrant sunset landscape painting.",
+      size:"Giclee,Edition Size: 45 + 5 Artist Proofs ,Image: 21 x 14"
+    },
+    moonlight: {
+      title: "Moonlight",
+      price: 250,
+      image: "images/moonlight.png",
+      desc: "A calm moonlit scenery on canvas.",
+      size:"Giclee,Edition Size: 45 + 5 Artist Proofs ,Image: 21 x 14"
+    }
+  };
